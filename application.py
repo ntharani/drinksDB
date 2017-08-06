bec7752e966a2d08bb56895f7f434a63d49baf70
Nagib Tharani <ntharani@gmail.com>
2017-08-06 11:36:50 -0700

    Fix: URL segment must not conflict with SQL data model. Ref: drink_familyURL_id in queries now

--- application.py
+++ application.py
@@ -30,62 +30,63 @@ def newDrink():
     return "NEW Drink Family Route!"
 
 #Edit a drink family
-@app.route('/drinks/<int:drink_family_id>/edit/', methods = ['GET', 'POST'])
-def editDrink(drink_family_id):
+@app.route('/drinks/<int:drink_familyURL_id>/edit/', methods = ['GET', 'POST'])
+def editDrink(drink_familyURL_id):
     return "EDIT Drink Family Route!"
 
 #Delete a drink family
-@app.route('/drinks/<int:drink_family_id>/delete/', methods = ['GET','POST'])
-def deleteDrink(drink_family_id):
+@app.route('/drinks/<int:drink_familyURL_id>/delete/', methods = ['GET','POST'])
+def deleteDrink(drink_familyURL_id):
     return "DELETE Drink Family Route!"
 
 #Show drink subtypes
-@app.route('/drinks/<int:drink_family_id>/')
-def showDrinkSubType(drink_family_id):
+@app.route('/drinks/<int:drink_familyURL_id>/')
+def showDrinkSubType(drink_familyURL_id):
     drinks = session.query(DrinkFamily).add_columns(DrinkFamily.name, DrinkFamily.id).order_by(asc(DrinkFamily.name))
-    subdrinks = session.query(DrinkSubType).add_columns(DrinkSubType.name, DrinkSubType.id).order_by(asc(DrinkSubType.name))    
-    print(drinks)
+    subdrinks = session.query(DrinkSubType).filter_by(drink_family_id = drink_familyURL_id ).add_columns(DrinkSubType.name, DrinkSubType.id, DrinkSubType.drink_family_id).order_by(asc(DrinkSubType.name))    
+    for subdrink in subdrinks:
+        print(subdrink)
     return render_template('drink_subtype_home.html', drinks = drinks, subdrinks = subdrinks)
     # return "SHOW Drink Family SubType Route!"
      
 #Create a new drink subtype item
-@app.route('/drinks/<int:drink_family_id>/new/',methods=['GET','POST'])
-def newDrinkSubType(drink_family_id):
+@app.route('/drinks/<int:drink_familyURL_id>/new/',methods=['GET','POST'])
+def newDrinkSubType(drink_familyURL_id):
     return "NEW Drink Family Subtype Route!"
 
 #Edit a drinkSubType
-@app.route('/drinks/<int:drink_family_id>/<int:type_id>/edit', methods=['GET','POST'])
-def editDrinkSubType(drink_family_id, type_id):
+@app.route('/drinks/<int:drink_familyURL_id>/<int:type_id>/edit', methods=['GET','POST'])
+def editDrinkSubType(drink_familyURL_id, type_id):
     return "EDIT Drink Family Subtype Route!"
 
 #Delete a drink SubType
-@app.route('/drinks/<int:drink_family_id>/<int:type_id>/delete', methods = ['GET','POST'])
-def deleteDrinkSubType(drink_family_id, type_id):
+@app.route('/drinks/<int:drink_familyURL_id>/<int:type_id>/delete', methods = ['GET','POST'])
+def deleteDrinkSubType(drink_familyURL_id, type_id):
     return "DELETE Drink Family SubType Route!"
 
 #Show drink subtype brands
-@app.route('/drinks/<int:drink_family_id>/<int:type_id>/')
-def showDrinkList(drink_family_id, type_id):
+@app.route('/drinks/<int:drink_familyURL_id>/<int:type_id>/')
+def showDrinkList(drink_familyURL_id, type_id):
     return "SHOW Drink Family SubType Drink List Route!"
 
 #Create a new drink subtype item
-@app.route('/drinks/<int:drink_family_id>/<int:type_id>/new/',methods=['GET','POST'])
-def newDrinkList(drink_family_id, type_id):
+@app.route('/drinks/<int:drink_familyURL_id>/<int:type_id>/new/',methods=['GET','POST'])
+def newDrinkList(drink_familyURL_id, type_id):
     return "NEW Drink Family Subtype Drink List Route!"
 
 #Edit a drinkSubType Brand
-@app.route('/drinks/<int:drink_family_id>/<int:type_id>/<int:drink_id>/edit', methods=['GET','POST'])
-def editDrinkList(drink_family_id, type_id, drink_id):
+@app.route('/drinks/<int:drink_familyURL_id>/<int:type_id>/<int:drink_id>/edit', methods=['GET','POST'])
+def editDrinkList(drink_familyURL_id, type_id, drink_id):
     return "EDIT Drink Family Subtype Drink List Route!"
 
 #Delete a drink SubType
-@app.route('/drinks/<int:drink_family_id>/<int:type_id>/<int:drink_id>/delete', methods = ['GET','POST'])
-def deleteDrinkList(drink_family_id, type_id, drink_id):
+@app.route('/drinks/<int:drink_familyURL_id>/<int:type_id>/<int:drink_id>/delete', methods = ['GET','POST'])
+def deleteDrinkList(drink_familyURL_id, type_id, drink_id):
     return "DELETE Drink Family SubType Drink List Route!"
 
 #Show drink subtype brand detail
-@app.route('/drinks/<int:drink_family_id>/<int:type_id>/<int:drink_id>/')
-def showDrinkListDetail(drink_family_id, type_id, drink_id):
+@app.route('/drinks/<int:drink_familyURL_id>/<int:type_id>/<int:drink_id>/')
+def showDrinkListDetail(drink_familyURL_id, type_id, drink_id):
     return "SHOW Drink Family SubType Drink List Item Detail Route!"
 
 
