a46dbb9a9756d221f2249cc68e0ba9cdf355d4a1
Nagib Tharani <ntharani@gmail.com>
2017-08-21 23:08:38 -0700

    Fixed social login with quoted user

--- __init__.py
+++ __init__.py
@@ -217,13 +217,13 @@ def createUser(login_session):
 
 
 def getUserInfo(user_id):
-    user = session.query("User").filter_by(id=user_id).one()
+    user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
 def getUserID(email):
     try:
-        user = session.query("User").filter_by(email=email).one()
+        user = session.query(User).filter_by(email=email).one()
         return user.id
     except:
         return None
